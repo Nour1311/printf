ca66829809141e6768aa751804930446b43e993b
Nour1311 <souici.mnh.991@gamil.com>
2023-08-22 11:06:19 -0700

    task 0 of printf

---
+++ _printf.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	va_list list;
+
+	if (format)
+	{
+		if (format[0] == ('\"') || (format[0] == ('\''))
+			{
+				while (format[i] != '%')
+				{
+					
+
+				}
+			}
+	}
+}
